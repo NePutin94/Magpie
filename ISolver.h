d24c1cdb881e43594222de4c562d812eba3da9c3
Dmitry <dimkap314@gmail.com>
2022-10-03 20:48:57 +0300

    Update ISolver.h

--- ISolver.h
+++ ISolver.h
@@ -1,5 +1,6 @@
 #ifndef MAGPIE_ISOLVER_H
 #define MAGPIE_ISOLVER_H
+#include <fstream>
 namespace Magpie
 {
     template<class T, class Result>
@@ -7,11 +8,19 @@ namespace Magpie
     {
     private:
     protected:
+        virtual std::string serialize() = 0;
+
+        virtual void deserialize(const std::string&) = 0;
+
     public:
         virtual Result solve() = 0;
 
-        void save()
-        {}
+        void save(std::string_view path)
+        {
+            std::ofstream out(path.data());
+            out << serialize();
+            out.close();
+        }
 
         void load()
         {}
